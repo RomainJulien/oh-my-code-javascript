b7e5ac0ef1a0dbaf1f47092b909d1a67fd68251e
romainjulien <romjulien.dev@gmail.com>
2025-04-18 13:05:16 +0200

    added exercice abonne

--- 2-abonne/script.js
+++ 2-abonne/script.js
@@ -4,8 +4,35 @@ console.log('connecté !');
 const icone = document.querySelector('i');
 console.log(icone);
 
-// je soumets à l'action
-icone.addEventListener('click', function() {
-  console.log('icone cliquée !');
-  icone.classList.toggle('fa-face-smile-wink');
-});
+const btnButton = document.querySelector('.btn');
+console.log(btnButton);
+
+// Fonction pour changer l'état (à la fois l'icône et le bouton)
+function toggleEtat() {
+  console.log('changement d\'état !');
+
+  // Change l'icône
+  if (icone.classList.contains('fa-meh-blank')) {
+    // Passer à l'état "abonné"
+    icone.classList.remove('fa-meh-blank');
+    icone.classList.add('fa-smile-wink');
+    icone.classList.add('happy');
+
+    // Changer le texte du bouton
+    btnButton.innerText = "Abonné";
+    btnButton.classList.add('abonne');
+  } else {
+    // Revenir à l'état initial "non abonné"
+    icone.classList.remove('fa-smile-wink');
+    icone.classList.add('fa-meh-blank');
+    icone.classList.remove('happy');
+
+    // Remettre le texte initial
+    btnButton.innerText = "Abonnez-vous";
+    btnButton.classList.remove('abonne');
+  }
+}
+
+// Attacher la fonction aux deux éléments
+icone.addEventListener('click', toggleEtat);
+btnButton.addEventListener('click', toggleEtat);
